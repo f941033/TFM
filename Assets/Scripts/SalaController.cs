a49e446ff2f15e52ad7cc588670c555596007e62
f941033 <f941033@gmail.com>
2025-03-21 09:22:48 +0100

    abrir solo sala contigua

--- Assets/Scripts/SalaController.cs
+++ Assets/Scripts/SalaController.cs
@@ -5,10 +5,17 @@ public class SalaController : MonoBehaviour
 {
     public Tilemap tilemap;
     Color originalColor;
+    public bool estaLibre = false;
+    public GameObject[] salasContiguas;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         originalColor = tilemap.color;
+        if (gameObject.tag == "salaCentral")
+        {
+            estaLibre = true;
+        }
     }
 
     // Update is called once per frame
@@ -19,17 +26,38 @@ public class SalaController : MonoBehaviour
 
     private void OnMouseDown()
     {
-        tilemap.gameObject.SetActive(false);
+        if (esSalaContigua())
+        {
+            tilemap.gameObject.SetActive(false);
+            estaLibre = true;
+        }
+            
     }
 
     private void OnMouseEnter()
     {
+        if (esSalaContigua())
+        {
+            tilemap.color = Color.yellow;
+            
+        }
         
-        tilemap.color = Color.yellow;
     }
 
     private void OnMouseExit()
     {
         tilemap.color = originalColor;
+    }
+
+    bool esSalaContigua()
+    {
+        foreach(GameObject sala in salasContiguas)
+        {
+            if(sala.GetComponent<SalaController>().estaLibre)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
