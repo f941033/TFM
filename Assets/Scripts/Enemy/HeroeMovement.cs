535f204e16721e4b55694d648773d34b1786c721
f941033 <f941033@gmail.com>
2025-06-26 14:25:55 +0200

    Heroe rompe cajas

--- Assets/Scripts/Enemy/HeroeMovement.cs
+++ Assets/Scripts/Enemy/HeroeMovement.cs
@@ -3,19 +3,40 @@ using System.Collections;
 
 public class HeroeMovement : MonoBehaviour
 {
+    Animator m_Animator;
+    private EnemyMovement movimientoScript;
+    float previousSpeed;
+
+    private void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        movimientoScript = GetComponent<EnemyMovement>();
+        previousSpeed = movimientoScript.moveSpeed;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("detectando muro exterior");
-
         if (collision.gameObject.tag == "PlayerWall")
         {
-            Debug.Log("detectando muro");
-            Invoke("Destruir", 2f);
+            Debug.Log("detectando caja");
+            m_Animator.SetBool("attacking", true);
+            StartCoroutine("DestroyBox", collision);
         }
     }
 
-    void Destruir()
+    IEnumerator DestroyBox(Collision2D collision)
     {
+        movimientoScript.moveSpeed = 0;
+        DestructibleWall box = collision.gameObject.GetComponent<DestructibleWall>();
+        int boxHealth = box.health;
+
+        for (int i = 1; i <= boxHealth; i++)
+        {
+            yield return new WaitForSeconds(1);
+            box.TakeDamage(1);
+        }
+
         Debug.Log("muro destruido");
+        m_Animator.SetBool("attacking", false);
+        movimientoScript.moveSpeed = previousSpeed;
     }
 }
