694086ffb3f51a94bc27d07877f54a760971d3b5
f941033 <f941033@gmail.com>
2025-07-16 20:39:13 +0200

    Trampas fijas de pinchos de madera en nuevas salas

---
+++ Assets/Scripts/Traps/FixedTrapBehaviour.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class FixedTrapBehaviour : MonoBehaviour
+{
+    public int damage;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            var enemy = collision.GetComponent<EnemyController>();
+            if (enemy == null) return;
+            enemy.ReceiveDamage(damage);
+        }
+    }
+}
