0a3291b6bce4bcf16f36ba32f1961ed5a7111098
f941033 <f941033@gmail.com>
2025-08-02 12:09:43 +0200

    arreglo bug salas poligonales

--- Assets/Scripts/Cards/ChangeCardPosition.cs
+++ Assets/Scripts/Cards/ChangeCardPosition.cs
@@ -29,7 +29,7 @@ public class ChangeCardPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (gameObject.name.Contains("TurretPrefab")) return;
+        if (gameObject.name.Contains("TurretPrefab")) return;
 
         if (estaArrastrando)
         {
