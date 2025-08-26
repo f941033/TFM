99c8a1648ab6d3167d81bdfb8689d2583905d65b
f941033 <f941033@gmail.com>
2025-08-26 15:00:36 +0200

    movimiento smooth de las cartas en mano en hover

--- Assets/Scripts/Cards/CardHoverInHand.cs
+++ Assets/Scripts/Cards/CardHoverInHand.cs
@@ -2,23 +2,31 @@ using DeckboundDungeon.GamePhase;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System.Collections;
 
 public class CardHoverInHand : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [Header("Configuración")]
-    public Color hoverColor = new Color(1f, 0.9f, 0.6f, 1f); // Color al hacer hover 
+    public Color hoverColor = new Color(1f, 0.9f, 0.6f, 1f);
     private Color originalColor;
 
     [Header("Referencias")]
-    public Image cardImage; // Asigna el componente Image de la carta
+    public Image cardImage;
     private CardSelector cardSelector;
 
-    // --------- NUEVO HOVER EN ESCALA -------------  //
+    // Hover en escala y animación
     private RectTransform rectTransform;
     private Vector3 originalScale;
+    private Vector3 hoverStartPosition;    // Guarda posición al entrar
+    private Vector3 targetHoverPosition;   // Posición a la que sube
     private float selecScale = 1.25f;
+    [SerializeField] private float hoverYOffset = 50f;
+    [SerializeField] private float animationDuration = 0.2f;
+    [SerializeField] private AnimationCurve easeCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
     private int indiceOriginal;
-    // ----------------------------------------------- //
+
+    private Coroutine currentAnimation;
+    private bool isHovering = false;
 
     void Awake()
     {
@@ -27,58 +35,64 @@ public class CardHoverInHand : MonoBehaviour, IPointerEnterHandler, IPointerExit
         originalColor = cardImage.color;
         cardSelector = GetComponent<CardSelector>();
 
-        // --------- NUEVO HOVER EN ESCALA -------------  //
         rectTransform = GetComponent<RectTransform>();
         originalScale = rectTransform.localScale;
-        // ----------------------------------------------- //
-
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        if (GameManager.CurrentPhase != GamePhase.Preparation &&
+            GameManager.CurrentPhase != GamePhase.Action)
+        {
+            if (!cardSelector.isSelected) cardImage.color = hoverColor;
+            return;
+        }
 
-        // --------- NUEVO HOVER EN ESCALA -------------  //
-        if (GameManager.CurrentPhase == GamePhase.Preparation || GameManager.CurrentPhase == GamePhase.Action)
-        {
-            indiceOriginal = transform.GetSiblingIndex();
-            transform.SetAsLastSibling();
-            rectTransform.localScale = originalScale * selecScale;
-            rectTransform.position += new Vector3(0f, 1f);
-        }
-        // ----------------------------------------------- //
-        else
-        {
-            if (!cardSelector.isSelected)
-                cardImage.color = hoverColor;
-        }
+        if (isHovering) return;
+        isHovering = true;
+
+        // Guardar datos antes de animar
+        hoverStartPosition = rectTransform.position;
+        targetHoverPosition = hoverStartPosition + Vector3.up * hoverYOffset;
+        indiceOriginal = transform.GetSiblingIndex();
+        transform.SetAsLastSibling();
+
+        if (currentAnimation != null) StopCoroutine(currentAnimation);
+        currentAnimation = StartCoroutine(Animate(hoverStartPosition, targetHoverPosition, originalScale, originalScale * selecScale));
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        // --------- NUEVO HOVER EN ESCALA -------------  //
-        if (GameManager.CurrentPhase == GamePhase.Preparation || GameManager.CurrentPhase == GamePhase.Action)
+        if (GameManager.CurrentPhase != GamePhase.Preparation &&
+            GameManager.CurrentPhase != GamePhase.Action)
         {
-            transform.SetSiblingIndex(indiceOriginal);
-            rectTransform.localScale = originalScale;
+            if (!cardSelector.isSelected) cardImage.color = originalColor;
+            return;
         }
-        // ----------------------------------------------- //
-        else
+
+        if (!isHovering) return;
+        isHovering = false;
+
+        // Restaurar orden y animar de regreso
+        transform.SetSiblingIndex(indiceOriginal);
+
+        if (currentAnimation != null) StopCoroutine(currentAnimation);
+        currentAnimation = StartCoroutine(Animate(rectTransform.position, hoverStartPosition, rectTransform.localScale, originalScale));
+    }
+
+    private IEnumerator Animate(Vector3 fromPos, Vector3 toPos, Vector3 fromScale, Vector3 toScale)
+    {
+        float elapsed = 0f;
+        while (elapsed < animationDuration)
         {
-            if (!cardSelector.isSelected)
-            cardImage.color = originalColor;
+            elapsed += Time.deltaTime;
+            float t = easeCurve.Evaluate(elapsed / animationDuration);
+            rectTransform.position = Vector3.Lerp(fromPos, toPos, t);
+            rectTransform.localScale = Vector3.Lerp(fromScale, toScale, t);
+            yield return null;
         }
+        rectTransform.position = toPos;
+        rectTransform.localScale = toScale;
+        currentAnimation = null;
     }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
 }
