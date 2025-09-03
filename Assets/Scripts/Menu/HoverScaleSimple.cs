456a574a00f6ad3b838694312ba7c5ac26f02765
f941033 <f941033@gmail.com>
2025-09-03 14:19:19 +0200

    sonidos de botones hover y clic en main menu

--- Assets/Scripts/Menu/HoverScaleSimple.cs
+++ Assets/Scripts/Menu/HoverScaleSimple.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class HoverScaleSimple : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class HoverScaleSimple : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+    public AudioSource audioSource;
+    public AudioClip clipHover, clipPressed;
+
     public float hoverScale = 1.3f;   // cuánto crece al pasar el ratón
     private Vector3 originalScale;
 
@@ -14,10 +17,21 @@ public class HoverScaleSimple : MonoBehaviour, IPointerEnterHandler, IPointerExi
     public void OnPointerEnter(PointerEventData e)
     {
         transform.localScale = originalScale * hoverScale;
+        audioSource.PlayOneShot(clipHover);
     }
 
     public void OnPointerExit(PointerEventData e)
     {
         transform.localScale = originalScale;
+    }
+
+    //public void SoundOnPressed()
+    //{
+    //    audioSource.PlayOneShot(clipPressed);
+    //}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        audioSource.PlayOneShot(clipPressed);
     }
 }
