1440a5d8f07571333483595a0b787c07cda35880
f941033 <f941033@gmail.com>
2025-12-06 16:57:12 +0100

    Escena Start con poster

---
+++ Assets/Scripts/Menu/StartScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
